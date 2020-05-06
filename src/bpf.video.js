42d36941d8c0093912f4df7d6a9e84168205f55f
blackpet <blackpet@gmail.com>
2020-05-06 13:08:02 +0900

    ES5 module system 적용

--- src/bpf.video.js
+++ src/bpf.video.js
@@ -5,8 +5,56 @@
  * @author blackpet
  * @date 2020.5.4
  */
+// import videojs from 'video.js'
+// import 'videojs-youtube'
+// import 'video.js/dist/video-js.css'
+// import '@videojs/themes/dist/fantasy/index.css'
+// import './bpf.video.css'
+const videojs = require('video.js')
+require('videojs-youtube')
+require('video.js/dist/video-js.css')
+require('@videojs/themes/dist/fantasy/index.css')
+require('./bpf.video.css')
 
-(function () {
-  console.log('wowwwwwwwwww~~~~');
-})();
+console.log('bpf.videojs started!!');
+
+
+let options = {
+  width: 800,
+  height: 450,
+  autoplay: true,
+  controls: true,
+  preload: 'auto',
+  playbackRates: [0.2, 0.5, 1, 1.2, 1.5, 2] // youtube only allows limits up to x2
+
+  , debug: false
+}
+
+
+function log(...args) {
+  if (options.debug) {
+    console.log(args);
+  }
+}
+
+
+
+
+// module.exports = {
+//   foo: () => console.log('foooooooo')
+// }
+
+function bpfvideojs(id, option) {
+  Object.assign(options, option)
+  log(options);
+
+  this.foo = () => {
+    console.log('fooooooooooo~');
+  }
+}
+
+module.exports = bpfvideojs
+
+
+
 
--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,56 @@ module.exports = (env, options) => {
     output: {
       filename: '[name].js',
       path: path.resolve(__dirname, 'dist'),
+      library: ['bpf', 'videojs'],
+      libraryTarget: 'umd',
+    },
+
+    externals: {
+      videojs: 'video.js'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/i,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-arrow-functions']
+            }
+          }
+        },
+
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
     }
   };
+
+  if (options.mode === 'development') {
+    // TODO blackpet: development settings...
+
+    config.devtool = 'inline-source-map';
+
+    // webpack-dev-server settings
+    config.devServer = {
+      contentBase: path.join(__dirname),
+      host: '0.0.0.0',
+      port: 4001,
+      hot: true,
+      // open: true,
+      proxy: {
+        '/admin': {
+          target: 'http://localhost:8080'
+        }
+      },
+    }
+  } else {
+    // TODO blackpet: production settings...
+  }
 
   return config;
 };
